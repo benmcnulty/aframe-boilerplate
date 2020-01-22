aba8ae0aa990c4fa2341dc301f65fa1b0529d751
Ben McNulty <benmcnulty@live.com>
2020-01-21 20:08:08 -0500

    Connected core A-Frame library and components

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,2 @@
 import '../css/style.scss';
-import moduleSection from '../js/body-module.mjs';
-
-const body = document.body;
-const defaultModuleSection = moduleSection();
-const dynamicModuleSection = moduleSection("Dynamic ES6 Module");
-
-body.appendChild(defaultModuleSection);
-body.appendChild(dynamicModuleSection);
+import components from '../js/components.js';
